968528c29cd08985cd46572d3fc621a7a3e1b3e1
Rik de Wit <rikdewit1997@gmail.com>
2020-03-20 19:17:33 +0100

    row the boat

--- main.js
+++ main.js
@@ -1,6 +1,172 @@
+var W = 640;
+var H = 480;
+var FR = 20;
+
+var boatimage;
+
+function preload(){
+    boatimage = loadImage('empacher.png');
+    paalimage = loadImage('paal.png');
+    frameRate(FR);
+}
+
 function setup() {
-    createCanvas(640, 480);
-  }
-  
-  function draw() {
-  }
+    createCanvas(W, H, WEBGL);
+    stroke(31);
+    strokeWeight(4);
+    noFill();
+    angleMode(DEGREES);
+
+    
+    imageMode(CENTER)
+    boat1 = new Boat(300-W/2, 300-H/2, boatimage, paalimage);
+}
+
+function draw() {
+    
+    background(206, 253, 253)
+    rect(-W/2, -H/2, W, H);
+    boat1.display()
+
+
+
+
+}
+
+
+function keyPressed(){
+    boat1.haal();
+}
+
+
+
+class Boat{
+    constructor(x, y, boatimage, paalimage){
+        this.x = x;
+        this.y = y;
+        this.xspeed = 0;
+        this.yspeed = 0;
+
+        this.h = 10;
+        this.w = 70;
+        this.boatimage = boatimage;
+
+        this.paal = new Paal(this.x, this.y, paalimage)
+        
+    }
+
+    display() {
+
+        
+        image(this.boatimage,this.x,this.y);
+        this.paal.update(this.x, this.y-20);
+        this.paal.display();
+        this.move()
+
+    }
+    move() {
+        
+        this.x += this.xspeed;
+        this.y += this.yspeed;
+    }
+
+    haal(){
+        this.paal.haal();
+    }
+
+
+}
+
+
+
+
+class Paal{
+    
+    constructor(x, y, image){
+        this.x = x;
+        this.y = y;
+        this.rspeed = 0;
+        this.rotation = 0;
+        this.zrotation = 0;
+
+        this.image = image;
+
+        this.w = this.image.width;
+        this.h = this.image.height;
+
+        this.inhaal = false;
+
+    }
+
+
+    display() {
+        
+
+        translate(this.x, this.y, 100)
+        rotate(180);
+        
+
+        //console.log(this.rotation)
+        console.log(this.zrotation)
+              
+        
+        rotateY(this.rotation);
+        rotateZ(this.zrotation)
+        this.rotate();
+        image(this.image,0,0);
+        
+        
+        
+        
+        
+    }
+    rotate() {
+        
+        this.rotation += this.rspeed;
+        
+
+    }
+
+    update(x, y){
+        this.x = x;
+        this.y = y;
+
+        if(this.inhaal){
+            this.rspeed = 10;
+            this.zrotation = 45-abs(this.rotation-90)/2;
+        }
+        if(this.rotation >= 180){
+            this.rspeed = 0;
+            
+        }
+    }
+
+    haal(){
+        if (!this.inhaal){
+            this.inhaal = true;
+        }else{
+            
+            this.inhaal = false;
+            this.rspeed = 0;
+            this.rotation = 0;
+            this.zrotation = 0;
+        }
+
+    }
+
+    recover(){
+        
+
+    }
+        
+        
+        
+        
+
+
+        
+
+        
+
+
+}
