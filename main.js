667fedd399fe27de08002dc2a8a8d90b5ffd02f8
Rik de Wit <rikdewit1997@gmail.com>
2020-03-20 23:22:58 +0100

    1v1

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ var W = 640;
 var H = 480;
 var FR = 60;
 
-var boatimage;
+var boatImage;
+var paalImage
 var backgroundImage;
 var bgpos = 0;
 
 function preload() {
-    boatimage = loadImage('empacher.png');
-    paalimage = loadImage('paal.png');
+    boatImage = loadImage('empacher.png');
+    paalImage = loadImage('paal.png');
     backgroundImage = loadImage('bg.jpg');
     frameRate(FR);
 }
@@ -18,69 +19,100 @@ function setup() {
     stroke(31);
     strokeWeight(4);
     noFill();
+    angleMode(RADIANS);
+    perspective(PI / 3.0, W / H, 0.1, 500);
+    //ortho(-W / 2, W / 2, -H / 2, H / 2, 1, 500);
     angleMode(DEGREES);
-    
+     
 
     imageMode(CENTER)
-    boat1 = new Boat(300 - W / 2, 330 - H / 2, boatimage, paalimage);
+    boat1 = new Boat(300 - W / 2, 330 - H / 2, 0, boatImage, paalImage);
+    boat2 = new Boat(300 - W / 2, 100 - H / 2, 1, boatImage, paalImage);
 }
 
 function draw() {
-    //console.log(boat1.xspeed);
-    background(206, 253, 253);
+    
+    orbitControl();
+     
+    
+    
     bgpos -= boat1.xspeed;
+
+
+    
+    background(206, 253, 253);
+
     push();
     scale(1.5);
+    
     translate(0,-10)
-    image(backgroundImage,bgpos,0);
+    image(backgroundImage,bgpos,0)
     image(backgroundImage,bgpos+W-50,0)
-
-    if(bgpos<-W){
+    pop();
+    if(bgpos<-W){ 
         bgpos = -50;
     }
-    pop();
 
     rect(-W / 2, -H / 2, W, H);
     boat1.display()
+    boat2.display()
+    boat2.xspeed = 7.5;
+    boat2.xspeed = boat2.xspeed-boat1.xspeed;
 
+    boat2.move()
+    
+    
+
+    
 
 }
 
 
 function keyPressed() {
     boat1.haal();
+    //boat2.haal();
 }
 
 
 
 class Boat {
-    constructor(x, y, boatimage, paalimage) {
+    constructor(x, y,speed, boatimage, paalimage) {
         this.x = x;
         this.y = y;
-        this.xspeed = 0;
+        this.xspeed = speed;
         this.yspeed = 0;
 
         this.h = 10;
         this.w = 70;
         this.boatimage = boatimage;
 
+        this.distance = 0;
+
         this.paal = new Paal(this.x, this.y, paalimage)
 
     }
  
     display() {
-
+        push();
+        translate(0,0, 150) 
+        scale(0.6)
         image(this.boatimage, this.x, this.y);
         
         this.paal.update(this.x, this.y - 50);
         this.paal.display();
-        console.log(this.paal.accel);
+        pop()
+
+        this.distance += this.xspeed;
+        //console.log(this.distance);
+        
+        //console.log(this.xspeed);
         this.xspeed += this.paal.accel*3;
-        this.xspeed;
+
         this.xspeed -= this.xspeed*this.xspeed*0.001 + 0.003;
         if(this.xspeed < 0){
             this.xspeed = 0;
         }
+        
         
 
     }
@@ -94,7 +126,7 @@ class Boat {
         
         this.paal.haal();
         console.log("inpik")
-        this.xspeed -= this.xspeed*0.01
+        this.xspeed -= this.xspeed*this.xspeed*0.01
 
 
     }
@@ -127,14 +159,14 @@ class Paal {
 
 
     display() {
-
-        translate(this.x, this.y, 100)
+        
+        translate(this.x, this.y, 30)
         rotate(180);
         rotateY(this.rotation);
         rotateZ(this.zrotation)
         this.rotate();
         image(this.image, 0, 0);
-
+        
     }
     rotate() {
         this.rotation += this.rspeed;
